418f2b9b5f20812542b31cad857d6e10ea49cefb
Ronye Vernaes <r.vernaes.ext-madiff@ogury.co>
2022-10-13 12:20:44 +0100

    stating all the grid service test cases

---
+++ vue/src/services/__tests__/GridService.spec.ts
@@ -0,0 +1,11 @@
+import { describe, it } from 'vitest';
+
+describe.skip('GridService', () => {
+  it('creates a random tile', () => {});
+  it('moves a tile towards up when the movement is up', () => {});
+  it('moves a tile towards down when the movement is down', () => {});
+  it('moves a tile towards left when the movement is left', () => {});
+  it('moves a tile towards right when the movement is right', () => {});
+  it('adds two tiles with the same value when moved in the same direction', () => {});
+  it('not add two tiles with different values when moved in the same direction', () => {});
+});
