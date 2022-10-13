6dcd848e8b7e8a7d771ca192542bb7edd6ca2ae7
Ronye Vernaes <r.vernaes.ext-madiff@ogury.co>
2022-10-13 08:22:33 +0100

    fixing tile broken test

--- vue/src/components/__tests__/Tile.spec.ts
+++ vue/src/components/__tests__/Tile.spec.ts
@@ -5,12 +5,12 @@ import Tile from '../Tile.vue';
 
 describe('Tile', () => {
   it("doesn't renders properly", () => {
-    const wrapper = mount(Tile, { props: { value: 0 } });
+    const wrapper = mount(Tile, { props: { data: { value: 0 } } });
     expect(wrapper.text()).not.toContain(0);
   });
 
   it('renders properly', () => {
-    const wrapper = mount(Tile, { props: { value: 2 } });
+    const wrapper = mount(Tile, { props: { data: { value: 2 } } });
     expect(wrapper.text()).toContain(2);
   });
 });
