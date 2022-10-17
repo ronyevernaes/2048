bdc436d9390f78b9ba23cd7dd79d016676a80b94
Ronye Vernaes <r.vernaes.ext-madiff@ogury.co>
2022-10-17 14:41:09 +0100

    adding tests to utils

--- vue/src/__tests__/utils.spec.ts
+++ vue/src/__tests__/utils.spec.ts
@@ -1,5 +1,13 @@
-import { describe, it, expect } from 'vitest';
-import { randomize, getMovementConfig } from '../utils';
+import { describe, it, expect, vi } from 'vitest';
+import {
+  randomize,
+  getMovementConfig,
+  getStart,
+  continueLoop,
+  changeLoopIndex,
+  delay,
+  generateId,
+} from '../utils';
 import { Axis, Command, Direction } from '../types';
 
 describe('randomize', () => {
@@ -16,12 +24,29 @@ describe('randomize', () => {
   });
 });
 
-describe.skip('generateId', () => {
-  it('returns a unique string', () => {});
+describe('generateId', () => {
+  it('returns a unique string', () => {
+    const now = Date.now();
+
+    vi.useFakeTimers();
+    vi.setSystemTime(now);
+
+    expect(generateId()).toBe(now.toString());
+    vi.useRealTimers();
+
+  });
 });
 
-describe.skip('delay', () => {
-  it('executes a callback after certain time', () => {});
+describe('delay', () => {
+  it('executes a callback after certain time', () => {
+    const callback = vi.fn(() => {});
+    delay(callback, 100);
+
+    const myInterval = setInterval(() => {
+      expect(callback).toHaveBeenCalled();
+      clearInterval(myInterval);
+    }, 150);
+  });
 });
 
 describe('getMovementConfig', () => {
@@ -52,23 +77,60 @@ describe('getMovementConfig', () => {
 
     expect(getMovementConfig(command)).toEqual(expected);
   });
+});
 
-  describe.skip('getStart', () => {
-    it('returns 0 when direction is Start', () => {});
-    it('returns N - 1 when direction is End', () => {});
+describe('getStart', () => {
+  it('returns 0 when direction is Start', () => {
+    const direction = Direction.Start;
+    expect(getStart(direction, 10)).toBe(0);
   });
 
-  describe.skip('continueLoop', () => {
-    it('returns true when index not reached last value for direction Start', () => {});
-    it('returns false when index reached last value for direction Start', () => {});
-    it('returns true when index not reached last value for direction End', () => {});
-    it('returns false when index reached last value for direction End', () => {});
+  it('returns N - 1 when direction is End', () => {
+    const direction = Direction.End;
+    expect(getStart(direction, 10)).toBe(10 - 1);
+  });
+});
+
+describe('continueLoop', () => {
+  it('returns true when index not reached last value for direction Start', () => {
+    const direction = Direction.Start;
+    expect(continueLoop(direction, 0, 10)).toBe(true);
   });
 
-  describe.skip('changeLoopIndex', () => {
-    it('moves the index towards the direction when direction Start', () => {});
-    it('moves the index against the direction when direction Start', () => {});
-    it('moves the index towards the direction when direction End', () => {});
-    it('moves the index against the direction when direction End', () => {});
+  it('returns false when index reached last value for direction Start', () => {
+    const direction = Direction.Start;
+    expect(continueLoop(direction, 10, 10)).toBe(false);
+  });
+
+  it('returns true when index not reached last value for direction End', () => {
+    const direction = Direction.End;
+    expect(continueLoop(direction, 9, 10)).toBe(true);
+  });
+
+  it('returns false when index reached last value for direction End', () => {
+    const direction = Direction.End;
+    expect(continueLoop(direction, 0, 10)).toBe(false);
+  });
+});
+
+describe('changeLoopIndex', () => {
+  it('moves the index towards the direction when direction Start', () => {
+    const direction = Direction.Start;
+    expect(changeLoopIndex(direction, 0)).toBe(1);
+  });
+
+  it('moves the index against the direction when direction Start', () => {
+    const direction = Direction.Start;
+    expect(changeLoopIndex(direction, 1, true)).toBe(0);
+  });
+
+  it('moves the index towards the direction when direction End', () => {
+    const direction = Direction.End;
+    expect(changeLoopIndex(direction, 1)).toBe(0);
+  });
+
+  it('moves the index against the direction when direction End', () => {
+    const direction = Direction.End;
+    expect(changeLoopIndex(direction, 0, true)).toBe(1);
   });
 });
